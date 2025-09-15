ac76eea2a47607405aca00db153efe7db677a7a0
mekoran <197427143+mekoran@users.noreply.github.com>
2025-09-15 11:45:37 -0400

    create winemacs

--- doom/myconfig.el
+++ doom/myconfig.el
@@ -1,46 +1,50 @@
-(global-aggressive-indent-mode 1)
-(add-to-list 'aggressive-indent-excluded-modes 'html-mode)
+;(global-aggressive-indent-mode 1)
+;(add-to-list 'aggressive-indent-excluded-modes 'html-mode)
 (setq-default tab-width 4)
 
 (setq doom-theme 'doom-oceanic-next)
 
 
 
-(setq org-directory "~/Documents/")
+(setq org-directory "~/org/")
 
 (setq org-todo-keywords
-      '((sequence "TODO" "DOING" "WAITING" "|" "DONE")
-        (sequence "ORDERED" "SHIPPED" "|" "COMPLETE")
+      '((sequence "TODO(t!)" "DOING(d!)" "|" "DONE(d!)")
+        (sequence "ORDERED(!)" "SHIPPED(!)" "|" "COMPLETE(!)")
+        (sequence "|" "CANCELED(c@/!)")
+        (sequence "WAITING(w@/!)")
+        (sequence "BLOCKED(b@/!)")
         ))
 (setq org-todo-keyword-faces
       '(("TODO" . org-warning)
         ("ORDERED" . "SlateBlue")
         ("DOING" . "DarkOrchid")
-        ("SHIPPED" . "DarkOrchid")
+        ("SHIPPED" . "Teal")
+        ("BLOCKED" . "Firebrick")
         ("WAITING" . "Teal")
         ("DONE" . "ForestGreen")
         ("COMPLETE" . "ForestGreen")
+        ("CANCELED" . "Grey")
         ))
 
 ;;(setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("laptop" . ?l)))
 
-(setq org-default-notes-file (concat org-directory "~/Documents/notes.org"))
+(setq org-default-notes-file (concat org-directory "~/org/notes.org"))
 
 (after! org
   (setq org-capture-templates
         (setq org-capture-templates
-               '(("t" "Todo" entry (file+headline "~/Documents/todo.org" "Tasks")
-                   "* TODO %?\n  %u %a"
+               '(("t" "Todo" entry (file+headline "~/org/todo.org" "Tasks")
+                   "* TODO %?\n %u %a"
                    :empty-lines 0
                    :kill-buffer t)
-                  ("j" "Journal" entry (file+datetree "~/Documents/journal.org")
-                   "* %?\nEntered on %U\n  >%i\n  %a"
+                  ("j" "Journal" entry (file+datetree "~/org/journal.org")
+                   "* %?\nEntered on %U\n >%i\n %a"
                    :empty-lines 0
                    :kill-buffer t)
-                  ("s" "Shortcut" table-line (file+headline "~/Documents/ref-tables.org" "keybinds")
-                   "|%?| | %^{system|Emacs|niri} |")
-    )))
-)
+                  ("s" "Shortcut" table-line (file+headline "~/org/ref-tables.org" "keybinds")
+                   "| %? |  |  |")
+    ))))
 ;(setq org-capture-templates-contexts
 ;      '(("c" ((in-mode . "message-mode")))
 ;        ("d" (my-context-function (in-mode . "org-mode")))
